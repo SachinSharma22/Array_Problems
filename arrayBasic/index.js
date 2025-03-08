9d21db2bc3a1912266162c997d24ec3235ffcfac
SachinSharma22 <s830sharma@gmail.com>
2025-03-08 10:29:00 +0530

    forEach used in iterator

--- arrayBasic/index.js
+++ arrayBasic/index.js
@@ -18,3 +18,12 @@ console.log(fruits[8]);
 fruits.length = 2;
 console.log(Object.keys(fruits));
 console.log(fruits.length);
+
+// forEach()
+const colors = ["red", "yellow", "blue"];
+colors[5] = "purple";
+colors.forEach((item, index) =>{
+    console.log(`${index}: ${item}`);
+});
+
+console.log(colors.reverse());
