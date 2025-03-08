63fb814702fe7f05fba184fd7ce28139cde916de
SachinSharma22 <s830sharma@gmail.com>
2025-03-08 11:36:56 +0530

    Added some extra function

--- arrayBasic/index.js
+++ arrayBasic/index.js
@@ -60,3 +60,64 @@ console.log(cities);
 const newLength = cities.push("Bristol");
 console.log(cities);
 console.log(newLength);
+
+// To add an item to the start of the array, use unshift():
+
+cities.unshift("Rasra");
+console.log(cities);
+
+// To remove the last item from the array, use pop().
+cities.pop();
+console.log(cities);
+
+// To remove the first item from an array, use shift():
+
+cities.shift();
+console.log(cities);
+
+// If you know the index of an item, you can remove it from the array using splice():
+
+const index = cities.indexOf("Liverpool");
+if(index != -1){
+    cities.splice(index, 1);//1 indicate how many item you want to remove from the array with the help of splice
+}
+console.log(cities);
+
+// Accessing every item
+
+const birds1 = ["Parrot", "Falcon", "Owl"];
+
+for(const bird of birds1){
+    console.log(bird);
+}
+
+// map()
+function double(number){
+    return number * 2;
+}
+
+const numbers = [1,2,3,4];
+const doubled = numbers.map(double);
+console.log(doubled);
+
+// filter()
+
+function isLong(city){
+    return city.length>8;
+}
+
+const longer = cities.filter(isLong);
+console.log(longer);
+
+// split()
+const data = "Manchester,London,Liverpool,Birmingham,Leeds,Carlisle";
+const city = data.split(",");
+console.log(city);
+city.length;
+city[0]; // the first item in the array
+city[1]; // the second item in the array
+city[cities.length - 1]; 
+
+const dogNames = ["Rocket", "Flash", "Bella", "Slugger"];
+dogNames.toString();
+console.log(dogNames);
