52a04fa9db1e1c9fc3f54dd039dd34e9895e1cbc
SachinSharma22 <s830sharma@gmail.com>
2025-03-08 10:21:15 +0530

    Basic of Arrays

---
+++ arrayBasic/index.js
@@ -0,0 +1,15 @@
+const fruits = [];
+fruits.push("banana","apple", "peach");
+console.log(fruits.length);
+
+fruits[5] = "mango";
+console.log(fruits[5]);
+console.log(Object.keys(fruits));
+console.log(fruits.length);
+
+
+fruits.length = 10;
+console.log(fruits);
+console.log(Object.keys(fruits));
+console.log(fruits.length);
+console.log(fruits[8]);
